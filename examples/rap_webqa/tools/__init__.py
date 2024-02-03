59d92ac8bce27ef5d36eab0a60d553d5e1533643
Andreas Binder <andreasjosef.binder@gmail.com>
2024-02-03 18:18:23 +0000

    refactoring

--- examples/rap_webqa/tools/__init__.py
+++ examples/rap_webqa/tools/__init__.py
@@ -1,10 +1,9 @@
-#from .retrieval import TextRetrieval as Retrieval, RetrievalResult
-#from .retrieval import RetrievalResult
-#from .retrieval import TextRetrieval as Retrieval
-#from .clip_ib_retrieval import ClipRetrieval as Retrieval
-#from .clip_ib_retrieval import ClipRetrieval as Retrieval
-#from .retrieval import Retrieval
-#from .clip_ib_retrieval_v2 import ClipRetrieval as Retrieval
+from .retrieval_base import MPNetRetrieval, ClipRetrieval, RetrievalResult, Query
 
-from .retrieval_base import MPNetRetrieval as Retrieval, RetrievalResult
-#from .retrieval_base import ClipRetrieval as Retrieval, RetrievalResult
+def get_retriever(model_name):
+    if model_name == 'mpnet':
+        return MPNetRetrieval
+    elif model_name == 'clip':
+        return ClipRetrieval
+    else:
+        raise ValueError("Unknown retrieval model")
